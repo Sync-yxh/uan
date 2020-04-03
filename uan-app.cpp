1b828f394a077f6af4b911aa199d2e27c8273783
yxh <836343716@qq.com>
2020-04-03 23:44:10 +0800

    init

--- uan-app.cpp
+++ uan-app.cpp
@@ -8,26 +8,49 @@
 #include "uan-hardware.h"
 #include "uan-route.h"
 
-void AppSend(char Des_ID,AppData inputAD)
+void SendAppData
 {
-	BundleData tempBD;
-	tempBD.Bundleflag = 1;
-	tempBD.Des_ID = Des_ID;
-	tempBD.Sou_ID = m_ID;
-	tempBD.bAD = inputAD;
-	BundleSendBuff.push_back(tempBD);
-	BundleWaitSend++;
+	RouteData Rdata;
+	Rdata.flag = 1;
+	Rdata.seqNum = dataSeqNum;
+	Rdata.srcID = m_ID;
+	AppData oridata;
+	oridata.Datalenth = 5;
+	for(int i=0;i<oridata.Datalenth;i++){
+		oridata.Data[i] = i+1
+	}
+	Rdata.data = oridata;
+	pthread_mutex_lock(&mut);
+	RouteWaitSend++;
+	RouteSendBuff.push_back( Rdata );
+	pthread_mutex_unlock(&mut);
 }
 
-bool AppRecv(AppData* outputAD)
+void SendAppState
 {
-	if(DataBuff.empty())
-		return false;
-	else
-	{
-		auto iter = DataBuff.begin();
-		DataBuff.pop_front();
-		*outputAD = *iter;
-	}
-	return true;
+	
+}
+
+void ScheduleData()
+{
+	struct itimerval tick;
+	signal(SIGALRM, SendAppData);				//接收到SIGALRM信号就计数
+	memset(&tick, 0, sizeof(tick));             //tick清零
+	tick.it_value.tv_sec = 1;                   //1秒后启动定时器
+	tick.it_value.tv_usec = 0;
+	tick.it_interval.tv_sec = 2;                //每隔2s发送一次SIGALRM信号
+    tick.it_interval.tv_usec = 0;
+	setitimer(ITIMER_REAL, &tick, NULL);        //ITIMER_REAL : 以系统真实的时间来计算，它送出SIGALRM信号
+}
+
+void ScheduleState()
+{
+	struct itimerval tick;
+	signal(SIGALRM, SendAppState);				//接收到SIGALRM信号就计数
+	memset(&tick, 0, sizeof(tick));             //tick清零
+	tick.it_value.tv_sec = 0;                   //0秒后启动定时器
+	tick.it_value.tv_usec = 0;
+	tick.it_interval.tv_sec = 300;                //每隔5m发送一次SIGALRM信号
+    tick.it_interval.tv_usec = 0;
+	setitimer(ITIMER_REAL, &tick, NULL);        //ITIMER_REAL : 以系统真实的时间来计算，它送出SIGALRM信号
 }
